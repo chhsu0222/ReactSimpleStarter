3805a4b1b73712e9ca6c71434bf2ea42d2c1e1a3
ch <ch@mail.startek-eng.com>
2018-11-19 13:56:12 +0800

    225. Export Statements.
    Make sure that we import react into all of our components
    that we write that have JSX.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyClJZQAHtMnbtTZdZ-MH6OQGvM5vp9hrGY';
 
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
-    return <div>Hi!</div>; // JSX
-}
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 // Take this component's generated HTML and put it 
 // on the page (in the DOM)
