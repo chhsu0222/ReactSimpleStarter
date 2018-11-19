87e42362058eb65746c598dcbe8a1eabbdd946f9
ch <ch@mail.startek-eng.com>
2018-11-19 11:36:39 +0800

    224. Youtube API Signup.
    Generate API key.
    $ npm i --save youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyClJZQAHtMnbtTZdZ-MH6OQGvM5vp9hrGY';
+
 // Create a new component. This component should produce
 // some HTML
 const App = () => {
